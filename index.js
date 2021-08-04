a2570cd68f609d1a2f5360cc04d60dc3c0b3e821
jucetina <juliethcetina@gmail.com>
2021-08-04 12:51:06 -0500

    Se agrega el responsive

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
 window.addEventListener('load', function(){
     new Glider(document.querySelector('.glider'), {
-        slidesToShow: 5,
+        slidesToShow: 1,
         slidesToScroll: 1,
         dots: '.dots',
         arrows: {
           prev: '.glider-prev',
           next: '.glider-next'
-        }
+        },
+        responsive: [
+          {
+            // screens greater than >= 480px
+            breakpoint: 480,
+            settings: {
+              // Set to `auto` and provide item width to adjust to viewport
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            // screens greater than >= 768px
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1
+            }
+          },
+          {
+            // screens greater than >= 1024px
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 1
+            }
+          },
+          {
+            // screens greater than >= 1200px
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 5,
+              slidesToScroll: 1
+            }
+          }
+        ]
     })
   })
